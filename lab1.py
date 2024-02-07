e786110e52e7904a3aa308a47a0deb43239d2be3
annegk0 <151765117+annegk0@users.noreply.github.com>
2024-02-07 16:35:57 +0100

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,106 @@
+from torchvision.datasets import CIFAR10
+import numpy as np 
+import torchvision.transforms as transforms
+import torch 
+from torch.utils.data.dataloader import DataLoader
+
+import sys
+sys.path.append('efficient-deep-learning/models_cifar100')
+import preact_resnet as pr
+
+
+
+## Normalization adapted for CIFAR10
+normalize_scratch = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+
+# Transforms is a list of transformations applied on the 'raw' dataset before the data is fed to the network. 
+# Here, Data augmentation (RandomCrop and Horizontal Flip) are applied to each batch, differently at each epoch, on the training set data only
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+### The data from CIFAR10 will be downloaded in the following folder
+rootdir = './data/cifar10'
+
+c10train = CIFAR10(rootdir,train=True,download=True,transform=transform_train)
+c10test = CIFAR10(rootdir,train=False,download=True,transform=transform_test)
+
+
+#For all the data
+
+trainloader = DataLoader(c10train,batch_size=32,shuffle=True)
+testloader = DataLoader(c10test,batch_size=32) 
+
+
+## number of target samples for the final dataset
+num_train_examples = len(c10train)
+num_samples_subset = 15000
+
+## We set a seed manually so as to reproduce the results easily
+seed  = 2147483647
+
+## Generate a list of shuffled indices ; with the fixed seed, the permutation will always be the same, for reproducibility
+indices = list(range(num_train_examples))
+np.random.RandomState(seed=seed).shuffle(indices)## modifies the list in place
+
+## We define the Subset using the generated indices 
+c10train_subset = torch.utils.data.Subset(c10train,indices[:num_samples_subset])
+print(f"Initial CIFAR10 dataset has {len(c10train)} samples")
+print(f"Subset of CIFAR10 dataset has {len(c10train_subset)} samples")
+
+# Finally we can define anoter dataloader for the training data
+trainloader_subset = DataLoader(c10train_subset,batch_size=32,shuffle=True)
+### You can now use either trainloader (full CIFAR10) or trainloader_subset (subset of CIFAR10) to train your networks.
+
+model = pr.PreActResNet18()
+
+n_epochs=2
+
+import torch.optim as optim
+import torch.nn as nn
+
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+
+best_loss = 100
+
+for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader_subset, 0): #les minibatchs sont de taille 32
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        if loss<best_loss :
+            best_loss = loss
+            state = {
+                'net': model.state_dict(),
+                #'hyperparam': hparam_currentvalue
+            }
+            torch.save(state, 'bestmodel'+epoch+i+'.pth')
+
+        loss.backward()
+        optimizer.step()
+        print(i)
+        # print statistics
+        running_loss += loss.item()
+        if i % 100 == 99:    # print every 100 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+print('Finished Training')
